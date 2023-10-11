1ede8b4fc60a26d9f300c7055f34abd35401959c
ani2689 <ojno7777@gmail.com>
2023-10-11 19:40:46 +0900

    :sparkles: 제품 entity 생성

---
+++ src/main/kotlin/team/untitled/chagocchagocbackend/domain/product/domain/entity/Product.kt
@@ -0,0 +1,30 @@
+package team.untitled.chagocchagocbackend.domain.product.domain.entity
+
+import javax.persistence.*
+
+@Entity
+class Product (
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    val id: Long,
+
+    @Column
+    val name: String,
+
+    @Column
+    val profile: String,
+
+    @Column(name = "wholesale_price")
+    val wholesalePrice: Int,
+
+    @Column(name = "retail_price")
+    val retailPrice: Int,
+
+    @OneToMany(cascade = [CascadeType.ALL])
+    @JoinColumn(name = "product_id")
+    val wholesale: List<Wholesale>,
+
+    @OneToMany(cascade = [CascadeType.ALL])
+    @JoinColumn(name = "product_id")
+    val reataile: List<Retail>,
+)
