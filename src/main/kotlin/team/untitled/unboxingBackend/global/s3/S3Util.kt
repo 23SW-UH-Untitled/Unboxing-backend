96804d782cb030a140d5cec6342c3f5ee85a3c16
ani2689 <ojno7777@gmail.com>
2023-10-11 21:22:09 +0900

    :sparkles: s3 관련 설정

---
+++ src/main/kotlin/team/untitled/unboxingBackend/global/s3/S3Util.kt
@@ -0,0 +1,44 @@
+package team.untitled.unboxingBackend.global.s3
+
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.internal.Mimetypes
+import com.amazonaws.services.s3.model.CannedAccessControlList
+import com.amazonaws.services.s3.model.ObjectMetadata
+import com.amazonaws.services.s3.model.PutObjectRequest
+import org.springframework.stereotype.Component
+import java.io.File
+import java.io.IOException
+import java.util.*
+
+@Component
+class S3Util(
+    private val s3Properties: S3Properties,
+    private val amazonS3: AmazonS3
+){
+    fun uploadFile(file: File): String {
+        val fileName = "${UUID.randomUUID()}.${file.extension}"
+
+        return inputS3(file, fileName)
+            .run { getResourceUrl(fileName = fileName) }
+    }
+
+    private fun inputS3(file: File, fileName: String) {
+        val objectMetadata = ObjectMetadata()
+        objectMetadata.contentLength = file.length()
+        objectMetadata.contentType = Mimetypes.getInstance().getMimetype(file)
+        try {
+            amazonS3.putObject(
+                PutObjectRequest(s3Properties.s3Bucket, fileName, file.inputStream(), objectMetadata)
+                    .withCannedAcl(
+                        CannedAccessControlList.PublicRead
+                    )
+            )
+            file.delete()
+        } catch (e: IOException) {
+            throw RuntimeException()
+        }
+    }
+
+    fun getResourceUrl(fileName: String): String =
+        amazonS3.getUrl(s3Properties.s3Bucket, fileName).toString()
+}
