d796341a351c8806ece9d75cdde7ee7104306af1
qlido <jason7530@naver.com>
2023-10-12 11:35:09 +0900

    fix: 에러처리

--- src/main/kotlin/team/untitled/unboxingBackend/domain/user/presentation/UserController.kt
+++ src/main/kotlin/team/untitled/unboxingBackend/domain/user/presentation/UserController.kt
@@ -1,12 +1,14 @@
 package team.untitled.unboxingBackend.domain.user.presentation
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team.untitled.unboxingBackend.domain.user.User
 import team.untitled.unboxingBackend.domain.user.presentation.dto.UserRes
 import team.untitled.unboxingBackend.global.utils.SecurityUtils
 
 @RestController
+@RequestMapping("/user")
 class UserController(
 ) {
     @GetMapping("me")
