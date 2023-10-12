fac5a7c24ae63afffd7fa047a79458b0b9048549
ani2689 <ojno7777@gmail.com>
2023-10-12 10:13:36 +0900

    :sparkles: data 생성

---
+++ src/main/kotlin/team/untitled/unboxingBackend/domain/product/controller/data/req/RetailProductReqData.kt
@@ -0,0 +1,6 @@
+package team.untitled.unboxingBackend.domain.product.controller.data.req
+
+data class RetailProductReqData(
+    val id: Long,
+    val count: Int
+)
--- src/main/kotlin/team/untitled/unboxingBackend/domain/product/controller/data/res/QueryProductListResData.kt
+++ src/main/kotlin/team/untitled/unboxingBackend/domain/product/controller/data/res/QueryProductListResData.kt
@@ -1,6 +1,7 @@
 package team.untitled.unboxingBackend.domain.product.controller.data.res
 
 data class QueryProductListResData(
+    val id: Long,
     val profile: String,
     val inventory: Int,
     val name: String,
