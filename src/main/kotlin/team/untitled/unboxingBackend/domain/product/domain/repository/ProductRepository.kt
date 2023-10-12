f206a6eb10fe640a21f3c6522d1b380f67f2a975
ani2689 <ojno7777@gmail.com>
2023-10-12 10:14:37 +0900

    :sparkles: findByUser

--- src/main/kotlin/team/untitled/unboxingBackend/domain/product/domain/repository/ProductRepository.kt
+++ src/main/kotlin/team/untitled/unboxingBackend/domain/product/domain/repository/ProductRepository.kt
@@ -3,6 +3,9 @@ package team.untitled.unboxingBackend.domain.product.domain.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import team.untitled.unboxingBackend.domain.product.domain.entity.Product
+import team.untitled.unboxingBackend.domain.user.User
 
 @Repository
-interface ProductRepository: JpaRepository<Product, Long>
+interface ProductRepository: JpaRepository<Product, Long> {
+    fun findByUser(user: User): List<Product>
+}
